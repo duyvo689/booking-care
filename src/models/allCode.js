ecd2899fb9c74aadb6520c26becf1387cc6a4a1d
duyvo689 <vothanhduy689@gmail.com>
2021-09-19 13:33:55 +0700

    update name file

--- src/models/allCode.js
+++ src/models/allCode.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Connection extends Model {
+  class allCode extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Connection.init(
+  allCode.init(
     {
       key: DataTypes.STRING,
       type: DataTypes.STRING,
@@ -19,8 +19,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "Connection",
+      modelName: "allCode",
     }
   );
-  return Connection;
+  return allCode;
 };
