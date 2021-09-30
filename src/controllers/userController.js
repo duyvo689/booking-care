69c738b273bfc8f1cb9b2957dc0aad3fa1c7a963
Nhon <nguyennhon2192@gmail.com>
2021-09-30 21:34:51 +0700

    api login p1-checkusername/password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,23 @@
-let handleLogin = (req,res) =>
-{
+import userService from "../services/userservices";
+
+let handleLogin = async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    //kiểm tra email và password đã được điền chưa
+    if (!email || !password) {
+        return res.status(500).json({
+            maloi: 1,
+            thongbao: 'Điền đầy đủ email & password'
+        })
+    }
+    let userData = await userService.handleUserLogin(email, password);
     return res.status(200).json(
         {
-            message: 'hellooooo'
+            maloi: userData.maloi,
+            thongbao: userData.thongbao,
+            user: userData.user ? userData.user : { 'thong bao': 'Khong hien thi thong tin user' }
         }
-    );
+    )
 }
 module.exports = {
     handleLogin: handleLogin
