24227698bb8429e38537607ad2236e60a4255e53
Nhon <nguyennhon2192@gmail.com>
2021-10-01 22:43:22 +0700

    CRUD-post

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,6 @@
 
 import db from "../models/index"
+import CRUDServices from "../services/CRUDServices"
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll()
@@ -10,8 +11,19 @@ let getHomePage = async (req, res) => {
         console.log(e)
     }
 
-}
+};
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs'); //render file giao diện nhập dữ liệu
+}
+let postCRUD = async (req, res) => {
+
+    await CRUDServices.createNewUser(req.body);
+    console.log(req.body);
+    return res.send('Đã thêm user');
+}
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
